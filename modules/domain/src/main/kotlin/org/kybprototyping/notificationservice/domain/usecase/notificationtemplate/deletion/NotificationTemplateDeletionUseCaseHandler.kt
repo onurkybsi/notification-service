81271b04de99fe6bd24302a479d88630fe7a90d3
Onur Kayabasi <o.kayabasi@outlook.com>
2024-06-16 01:35:46 +0300

    db default values set

--- modules/domain/src/main/kotlin/org/kybprototyping/notificationservice/domain/usecase/notificationtemplate/deletion/NotificationTemplateDeletionUseCaseHandler.kt
+++ modules/domain/src/main/kotlin/org/kybprototyping/notificationservice/domain/usecase/notificationtemplate/deletion/NotificationTemplateDeletionUseCaseHandler.kt
@@ -5,7 +5,7 @@ import org.kybprototyping.notificationservice.domain.common.interfaces.InputOnly
 import org.kybprototyping.notificationservice.domain.common.exception.nonExistentData
 import org.kybprototyping.notificationservice.domain.common.interfaces.Transactional
 
-internal class NotificationTemplateDeletionUseCaseHandler(
+internal open class NotificationTemplateDeletionUseCaseHandler(
     private val notificationTemplateRepositoryAdapter: NotificationTemplateRepositoryPort
 ): InputOnlyUseCaseHandler<Int> {
 
