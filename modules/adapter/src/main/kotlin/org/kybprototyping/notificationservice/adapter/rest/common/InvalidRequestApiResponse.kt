12c1665efc0b8bc1ec8b4fc1f5fe04fe4d655722
Onur Kayabasi <o.kayabasi@outlook.com>
2024-09-29 12:36:36 +0200

    InvalidRequestResponse added

---
+++ modules/adapter/src/main/kotlin/org/kybprototyping/notificationservice/adapter/rest/common/InvalidRequestApiResponse.kt
@@ -0,0 +1,22 @@
+package org.kybprototyping.notificationservice.adapter.rest.common
+
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.http.MediaType
+import org.springframework.http.ProblemDetail
+
+@ApiResponse(
+    responseCode = "400",
+    content = [
+        Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ProblemDetail::class)
+        )
+    ]
+)
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class InvalidRequestResponse(
+    val description: String = "Invalid request"
+)
